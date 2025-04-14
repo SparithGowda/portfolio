eb0c3b8c1de1e5f2eadb52f70f7e3259ea92a111
SpGowdarith <158543947+SparithGowda@users.noreply.github.com>
2025-04-14 14:38:47 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -77,7 +77,7 @@ function Navbar() {
                   className="flex items-center px-4 py-2 hover:bg-gray-100 transition text-gray-800"
                 >
                    <img
-                    src={linkedin}
+                    src=portfolio/{linkedin}
                     alt="LinkedIn"
                     className="w-5 h-5"
                   />
